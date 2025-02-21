d99568ab1e3662d35e7a7dec053520dbd5af14d5
Nguyen Phuoc <163439962+PhuocNguyen1234@users.noreply.github.com>
2025-02-21 15:00:42 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Tính S(n) = 1 + 1/2 + 1/3 + ... +1/n
+namespace Bai_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            float s;
+            s = 0;
+            Console.Write("Nhap vao so n: ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                s = s + 1.0f / i;
+            }
+            Console.Write("Ket qua cua phep tinh: 1 + 1/2 + ... + 1/{0} la: {1}", n, s);
+
+        }
+    }
+}
